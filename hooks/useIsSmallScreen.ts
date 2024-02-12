6b2b6ffdc85a99b3e98ac175b2642fd8fd998af6
cch137 <96408836+cch137@users.noreply.github.com>
2024-02-12 14:02:10 +0800

    fix: use store

--- hooks/useIsSmallScreen.ts
+++ hooks/useIsSmallScreen.ts
@@ -1,18 +1,29 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import store from "@cch137/utils/dev/store";
+import { useEffect, useState } from "react";
 
 export const SMALL_SCREEN_W = 720;
 
-export default function useIsSmallScreen() {
-  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
+const ref = store(
+  { value: false, _inited: false },
+  () => {
+    if (!ref.$inited) {
+      window.addEventListener("resize", ref.$update);
+    }
+    if (typeof window === "undefined") return { value: false };
+    return { value: window.innerWidth < SMALL_SCREEN_W };
+  },
+  { initAfterOn: true }
+);
 
-  useEffect(() => {
-    const detectSmallScreen = () => setIsSmallScreen(window.innerWidth < SMALL_SCREEN_W);
-    detectSmallScreen();
-    window.addEventListener('resize', detectSmallScreen);
-    return () => window.removeEventListener('resize', detectSmallScreen);
-  }, [setIsSmallScreen]);
+export default function useIsSmallScreen() {
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(ref.value);
+
+  useEffect(
+    () => ref.$on(({ value }) => setIsSmallScreen(value)),
+    [setIsSmallScreen]
+  );
 
   return isSmallScreen;
-};
+}
