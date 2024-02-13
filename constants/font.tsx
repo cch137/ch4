c0e0427e03186b221df58a733d372305cfe9705e
cch137 <96408836+cch137@users.noreply.github.com>
2024-02-13 15:17:46 +0800

    fix: font

--- constants/font.tsx
+++ constants/font.tsx
@@ -1,36 +1,40 @@
-import { Noto_Sans, Noto_Sans_SC, Noto_Sans_TC, Noto_Sans_HK, Noto_Sans_JP, Noto_Sans_KR } from 'next/font/google'
+import {
+  Noto_Sans,
+  Noto_Sans_SC,
+  Noto_Sans_TC,
+  Noto_Sans_HK,
+  Noto_Sans_JP,
+  Noto_Sans_KR,
+} from 'next/font/google'
 import type { NextFont } from 'next/dist/compiled/@next/font/dist/types'
 
-const notoSansSC = Noto_Sans_SC({subsets: ['latin']});
-const notoSansTC = Noto_Sans_TC({subsets: ['latin']});
-const notoSansHK = Noto_Sans_HK({subsets: ['latin']});
-const notoSansJP = Noto_Sans_JP({subsets: ['latin']});
-const notoSansKR = Noto_Sans_KR({subsets: ['latin']});
-const notoSans = Noto_Sans({subsets: ['latin']});
+const notoSansSC = Noto_Sans_SC({subsets: ['latin'], weight: '300'});
+const notoSansTC = Noto_Sans_TC({subsets: ['latin'], weight: '300'});
+const notoSansHK = Noto_Sans_HK({subsets: ['latin'], weight: '300'});
+const notoSansJP = Noto_Sans_JP({subsets: ['latin'], weight: '300'});
+const notoSansKR = Noto_Sans_KR({subsets: ['latin'], weight: '300'});
+const notoSans = Noto_Sans({subsets: ['latin'], weight: '400'});
 
 export const fonts = [
+  notoSans,
   notoSansTC,
   notoSansSC,
   notoSansHK,
   notoSansJP,
   notoSansKR,
-  notoSans,
 ];
 
-const styles = fonts.reduce(({fontFamilies, fontStyles}, {style: {fontFamily, fontStyle}}, i, a) => {
+const styles = fonts.reduce(({fontFamilies}, {style: {fontFamily}}, i, a) => {
   fontFamilies.add(fontFamily);
-  if (fontStyle) fontStyles.add(fontStyle);
-  return {fontFamilies, fontStyles};
+  return {fontFamilies};
 }, {
   fontFamilies: new Set<string>(),
-  fontStyles: new Set<string>(),
 });
 
 export const font: NextFont = {
   className: 'default-font',
   style: {
     fontFamily: [...styles.fontFamilies].join(',').replace(/,[\s]+/g, ','),
-    fontStyle: [...styles.fontStyles].join(' '),
   }
 }
 
