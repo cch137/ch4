a5f44aa9ebc2733479e5c4e47026375fdd4f4280
cch137 <96408836+cch137@users.noreply.github.com>
2024-02-15 16:59:58 +0800

    fix: 404 page navbar

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { appTitle } from "@/constants/app"
 import { Button } from "@nextui-org/button"
 import Link from "next/link"
+import AppNavbar from "./components/app-navbar"
 
 const description = 'This page could not be found.'
 
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (<>
-    <title>{appTitle('404 Not Found')}</title>
+    <AppNavbar />
     <div className="fixed z-50" style={{top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}}>
       <div className="flex-center flex-col gap-12">
         <div className="flex-center gap-4 text-default-700">
