427888634cef0700c2d1d1ac841e8645ad96a41d
cch137 <96408836+cch137@users.noreply.github.com>
2024-03-09 22:23:36 +0800

    fix: onContextMenu

--- app/view/harimau/[id]/page.tsx
+++ app/view/harimau/[id]/page.tsx
@@ -20,7 +20,7 @@ export default function HarimauView() {
   const title = link.split('?')[0];
   const url = `https://api.cch137.link/ls/i/${link}`;
 
-  return (<div className="w-full flex-center pointer-events-none">
+  return (<div className="w-full flex-center" onContextMenu={(e) => e.preventDefault()}>
     <title>{appTitle(title)}</title>
     <div className="max-w-full pointer-events-none">
       <Image
