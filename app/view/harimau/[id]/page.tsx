6957e6cae687d0e3b6aad05984ee843b495dc64e
cch137 <96408836+cch137@users.noreply.github.com>
2024-03-09 21:42:12 +0800

    fix: add pointer-events-none

--- app/view/harimau/[id]/page.tsx
+++ app/view/harimau/[id]/page.tsx
@@ -20,13 +20,14 @@ export default function HarimauView() {
   const title = link.split('?')[0];
   const url = `https://api.cch137.link/ls/i/${link}`;
 
-  return (<div className="w-full flex-center">
+  return (<div className="w-full flex-center pointer-events-none">
     <title>{appTitle(title)}</title>
-    <div className="max-w-full">
+    <div className="max-w-full pointer-events-none">
       <Image
         alt={url}
         src={url}
         className="rounded-none w-full select-none pointer-events-none"
+        classNames={{wrapper: 'pointer-events-none'}}
         draggable="false"
         style={{width: 870}}
       />
