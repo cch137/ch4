cc7667877e524a5a34cf36e0b759c1db135314e1
cch137 <96408836+cch137@users.noreply.github.com>
2024-03-23 16:47:28 +0800

    fix: remove smooth volume feature

--- app/view/silence/page.tsx
+++ app/view/silence/page.tsx
@@ -218,14 +218,8 @@ function AudioController({audio, currentMix, globalVolume = 1, speed = 1}: { aud
 
   useEffect(() => {
     if (audio.volume !== volume) audio.volume = volume;
-    const targetComputedVolume = volume * globalVolume;
-    const deltaVolume = targetComputedVolume - computedVolume;
-    if (Math.round(deltaVolume * 100) === 0) return;
-    const step = deltaVolume > 0 ? 1 : -1;
-    setTimeout(() => {
-      setComputedVolume(Math.max(0, Math.min(1, (Math.round(computedVolume * 100) + step) / 100)));
-    }, 10);
-  }, [globalVolume, audio, volume, computedVolume, setComputedVolume]);
+    setComputedVolume(volume * globalVolume);
+  }, [audio, volume, globalVolume, setComputedVolume]);
 
   useEffect(() => {
     setup();
@@ -236,6 +230,10 @@ function AudioController({audio, currentMix, globalVolume = 1, speed = 1}: { aud
   useEffect(() => {
     const load = () => {
       setVolume(audio.volume);
+      try {
+        mainRef.current!.currentTime = 0;
+        glueRef.current!.currentTime = 0;
+      } catch {}
     }
     et.addEventListener(LOAD_EVENT, load);
     et.addEventListener(PLAY_EVENT, play);
@@ -312,7 +310,6 @@ export default function Silence() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [globalVolume, setGlobalVolume] = useState(1);
-  const [computedGlobalVolume, setComputedGlobalVolume] = useState(1);
   const [globalSpeed, setGlobalSpeed] = useState(1);
   const [mixConfigList, setMixConfigList] = useState<MixConfig[]>([]);
   const [showRelaxingMusic, setShowRelaxingMusic] = useState(false);
@@ -338,15 +335,6 @@ export default function Silence() {
       save();
     }
   }, [mixConfigList, globalVolume, loaded]);
-
-  useEffect(() => {
-    const deltaVolume = (isPlaying ? globalVolume : 0) - computedGlobalVolume;
-    if (Math.round(deltaVolume * 100) === 0) return;
-    const step = deltaVolume > 0 ? 1 : -1;
-    setTimeout(() => {
-      setComputedGlobalVolume(Math.max(0, Math.min(1, (Math.round(computedGlobalVolume * 100) + step) / 100)));
-    }, 30);
-  }, [isPlaying, globalVolume, computedGlobalVolume, setComputedGlobalVolume]);
 
   useEffect(() => {
     const mix = mixConfigList.find(m => m.isPlaying);
@@ -569,7 +557,7 @@ export default function Silence() {
         {catrgorizedSources.map((cate, i) => (<div key={i}>
           <div className="text-2xl text-default-500 font-semibold">{cate.name}</div>
           <div className="flex flex-wrap gap-x-4 gap-y-2 p-1">
-            {cate.sources.map((s, i) => <AudioController audio={s} currentMix={mixConfigList.find(m => m.isPlaying)!} globalVolume={computedGlobalVolume} speed={globalSpeed} key={i} />)}
+            {cate.sources.map((s, i) => <AudioController audio={s} currentMix={mixConfigList.find(m => m.isPlaying)!} globalVolume={globalVolume} speed={globalSpeed} key={i} />)}
           </div>
           <Spacer y={4} />
           <Divider />
