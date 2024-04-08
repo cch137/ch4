e73d3af1fa93b0b4334a0e613f56fd229e54b7ec
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-08 23:03:20 +0800

    feat: wl

--- app/auth/wl/page.tsx
+++ app/auth/wl/page.tsx
@@ -5,5 +5,5 @@ import useTTX from "@/hooks/useTTX";
 
 export default function AuthUnblock() {
   const { ttxRecord } = useTTX();
-  return <FullpageSpinner redirectTo={`/`} callback={async () => ttxRecord('unblock')} />
+  return <FullpageSpinner redirectTo={`/`} callback={async () => prompt('What is your lucky number') === 'cch137' ? ttxRecord('wl') : null} />
 }
