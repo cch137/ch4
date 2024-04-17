d7cdd351eb63e81fb875c25276f5aeb408717ec8
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-18 02:44:31 +0800

    feat: text-ans focus to view

--- app/view/text-ans/[id]/page.tsx
+++ app/view/text-ans/[id]/page.tsx
@@ -7,7 +7,7 @@ import useUserInfo from "@/hooks/useUserInfo";
 import { unpackDataWithHash } from "@cch137/utils/shuttle";
 import { Image } from "@nextui-org/image";
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const parseLink = (id: string) => {
   try {
@@ -26,6 +26,11 @@ export default function TextAnsView() {
   const isMember = auth > 3;
   const title = isLink ? link.split('?')[0] : 'Unknown';
   const url = `https://api.cch137.link/ls/i/${link}`;
+  const [isFocus, setIsFocus] = useState(false);
+  const [isPressing, setIsPressing] = useState(false);
+  const [_isPressing, _setIsPressing] = useState(false);
+  const blur = (!isFocus || isPressing) && !isMember;
+  const notPressingTimeout = useRef<NodeJS.Timeout>()
 
   useEffect(() => {
     const initTextAnsView = () => {
@@ -33,9 +38,36 @@ export default function TextAnsView() {
       ttxRecord('text-ans-view', { link });
       ttxRecord('known-text-ans');
     }
+    const detectIsFocus = () => {
+      const isFocus = document.hasFocus();
+      setIsFocus(isFocus);
+      if (isFocus) setIsPressingF();
+    }
+    const setIsPressingT = () => {
+      clearTimeout(notPressingTimeout.current);
+      _setIsPressing(true);
+      setIsPressing(true);
+    }
+    const setIsPressingF = () => {
+      _setIsPressing(false);
+      notPressingTimeout.current = setTimeout(() => {
+        setIsPressing(false);
+      }, 1000);
+    }
+    detectIsFocus();
     window.addEventListener('TTX-view', initTextAnsView);
-    return () => window.removeEventListener('TTX-view', initTextAnsView);
-  }, [link, isLink, ttxRecord]);
+    window.addEventListener('focus', detectIsFocus);
+    window.addEventListener('blur', detectIsFocus);
+    window.addEventListener('keydown', setIsPressingT);
+    window.addEventListener('keyup', setIsPressingF);
+    return () => {
+      window.removeEventListener('TTX-view', initTextAnsView);
+      window.removeEventListener('focus', detectIsFocus);
+      window.removeEventListener('blur', detectIsFocus);
+      window.removeEventListener('keydown', setIsPressingT);
+      window.removeEventListener('keyup', setIsPressingF);
+    }
+  }, [link, isLink, ttxRecord, setIsFocus, setIsPressing, _setIsPressing, notPressingTimeout]);
 
   if (ttxBlock || !isLink) return <NotFound />;
 
@@ -50,12 +82,17 @@ export default function TextAnsView() {
         {ttxShow ? <Image
           alt={url}
           src={url}
-          className={`rounded-none w-full select-none ${isMember ? '' : 'pointer-events-none'}`}
+          className={`rounded-none w-full select-none ${isMember ? '' : 'pointer-events-none'} ${blur ? 'blur' : ''}`}
           classNames={{ wrapper: isMember ? '' : 'pointer-events-none' }}
           draggable="false"
           style={{ width: 960 }}
         /> : null}
       </div>
+      {blur ? <div className="fixed top-0 m-auto h-screen z-50 flex-center select-none">
+        <div className="text-default-300 text-2xl font-bold flex-center">
+          {(isPressing && !_isPressing) ? 'please wait' : isPressing ? 'keyup to focus' : 'click to focus'}
+        </div>
+      </div> : null}
     </div>
   )
 }
