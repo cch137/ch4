cce387c9ca7d02d1d0bc874a44388988f8c13f51
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-20 02:23:09 +0800

    feat: useIsLoggedIn

--- hooks/useUserInfo.ts
+++ hooks/useUserInfo.ts
@@ -1,33 +1,41 @@
-'use client';
+"use client";
 
-import { UserInfo, UserDetails, StatusResponse } from '@/constants/types';
-import store from '@cch137/utils/dev/store';
-import { useEffect, useState } from 'react';
+import { UserInfo, UserDetails, StatusResponse } from "@/constants/types";
+import store from "@cch137/utils/dev/store";
+import { useEffect, useState } from "react";
 
-export const userInfoStore = store({
-  id: '',
-  name: '',
-  auth: 0,
-} as UserInfo, async () => {
-  try {
-    const res = await fetch('/api/auth/user', {method: 'POST'});
-    return (await res.json() as StatusResponse<UserInfo>)?.value || {};
-  } catch {}
-}, {
-  autoInit: false,
-  initAfterOn: true,
-  updateInterval: 60 * 1000,
-});
+export const userInfoStore = store(
+  {
+    id: "",
+    name: "",
+    auth: 0,
+  } as UserInfo,
+  async () => {
+    try {
+      const res = await fetch("/api/auth/user", { method: "POST" });
+      return ((await res.json()) as StatusResponse<UserInfo>)?.value || {};
+    } catch {}
+  },
+  {
+    autoInit: false,
+    initAfterOn: true,
+    updateInterval: 60 * 1000,
+  }
+);
 
-export const userDetailsStore = store({} as UserDetails, async () => {
-  try {
-    const res = await fetch('/api/auth/user/details', {method: 'POST'});
-    return (await res.json() as StatusResponse<UserDetails>)?.value || {};
-  } catch {}
-}, {
-  autoInit: false,
-  initAfterOn: true,
-});
+export const userDetailsStore = store(
+  {} as UserDetails,
+  async () => {
+    try {
+      const res = await fetch("/api/auth/user/details", { method: "POST" });
+      return ((await res.json()) as StatusResponse<UserDetails>)?.value || {};
+    } catch {}
+  },
+  {
+    autoInit: false,
+    initAfterOn: true,
+  }
+);
 
 export const useUserInfo = () => {
   const [userInfo, setUserInfo] = useState(userInfoStore.$object);
@@ -37,6 +45,11 @@ export const useUserInfo = () => {
 
 export default useUserInfo;
 
+export const useIsLoggedIn = () => {
+  const userInfo = useUserInfo();
+  return userInfo.auth > 0;
+};
+
 export const useUserDetails = () => {
   const [userDetails, setUserDetails] = useState(userDetailsStore.$object);
   useEffect(() => userDetailsStore.$on(setUserDetails), []);
@@ -44,19 +57,30 @@ export const useUserDetails = () => {
 };
 
 export const useUserProfile = () => {
-  const {id, name, auth} = useUserInfo();
-  const {eadd, ctms, mtms, atms} = useUserDetails();
+  const { id, name, auth } = useUserInfo();
+  const { eadd, ctms, mtms, atms } = useUserDetails();
 
   return {
-    id, name, auth,
-    eadd, ctms, mtms, atms,
+    id,
+    name,
+    auth,
+    eadd,
+    ctms,
+    mtms,
+    atms,
     $init: async () => {
-      const [a, b] = await Promise.all([userInfoStore.$init(), userDetailsStore.$init()]);
-      return {...b, ...a};
+      const [a, b] = await Promise.all([
+        userInfoStore.$init(),
+        userDetailsStore.$init(),
+      ]);
+      return { ...b, ...a };
     },
     $update: async () => {
-      const [a, b] = await Promise.all([userInfoStore.$update(), userDetailsStore.$update()]);
-      return {...b, ...a};
+      const [a, b] = await Promise.all([
+        userInfoStore.$update(),
+        userDetailsStore.$update(),
+      ]);
+      return { ...b, ...a };
     },
     $inited: userInfoStore.$inited || userDetailsStore.$inited,
     $updating: userInfoStore.$updating || userDetailsStore.$updating,
